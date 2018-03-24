da5cd03c30979d7e4b4a89cf01b52282727502a6
Jitesh Middha <jiteshmiddha@ymail.com>
2018-03-24 13:05:26 +0530

    Initial Commit

---
+++ MusicDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicDemo
+//
+//  Created by Jitesh Middha on 24/03/18.
+//  Copyright © 2018 Jitesh Middha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
